ad136b34cba421e7e9d5156da21dfc28faf347ab
Rafael <123227224+RafaelHenriqu@users.noreply.github.com>
2025-02-04 01:57:18 -0300

    final update

--- Config/Configs.js
+++ Config/Configs.js
@@ -1,9 +1,9 @@
 const fs = require("fs")
 
-const LanguageArray = fs.readdirSync("./json/Language")
+const LanguageArray = ["en-us.json","pt-br.json","ru-ru.json","zh-cn.json"]
 
 const Settings = {
-    Language:require(`../json/Language/en-us.json`)
+    Language:require(`${__dirname}/json/Language/en-us.json`)
 }
 
 
@@ -12,7 +12,7 @@ const Settings = {
 function Set_Language(Language="en-us"){
     Language = Language + ".json"
     if (LanguageArray.includes(Language.toLowerCase())){
-        Settings.Language = require(`../json/Language/${Language}`) 
+        Settings.Language = require(`${__dirname}/json/Language/${Language}`) 
     }else{
         console.log(Settings.Language.Set_Language_ERROR)
     }
